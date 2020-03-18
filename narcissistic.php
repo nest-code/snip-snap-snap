a9542219727b4d9556053fa4ccd526887eb8960f
Ernesto Manurung <33409476+ernestoandreyulian@users.noreply.github.com>
2020-03-18 12:17:02 +0700

    update

---
+++ narcissistic.php
@@ -0,0 +1,36 @@
+<?php 
+function countDigit($n) 
+{ 
+	if ($n == 0) 
+		return 0; 
+
+	return (1 + countDigit($n / 10)); 
+} 
+
+
+function check( $n) 
+{ 
+
+	$l = countDigit($n); 
+	$dup = $n; 
+	$sum = 0; 
+
+	while ($dup) 
+	{ 
+		$sum += pow($dup % 10, $l); 
+		$dup = (int)$dup / 10; 
+	} 
+
+	return ($n == $sum); 
+} 
+
+
+$n = 153; 
+if (check(!$n)) 
+	echo "True"; 
+else
+	echo "no"; 
+
+
+
+?> 
--- neddle_in_the_haystack.php
+++ neddle_in_the_haystack.php
@@ -1,5 +1,8 @@
 <?php
+
 $findNeedle=array("red"=>"r","blue"=>"b","yellow"=>"y","black"=>"b","grey"=>"gr","blue"=>"bl");
 echo array_search("bl",$findNeedle);
+
+
 
 ?>
